ccb404de4b5d578722bccf5c1ab07f94717a0e14
giomax <kvavilashvili@gmail.com>
2024-03-06 21:27:04 +0400

    up

--- week01/main.py
+++ week01/main.py
@@ -1,12 +1,27 @@
 import math
 import ipaddress
 
-
+#N1
 print('giorgi')
 
-a = 5
-b = 6
-print(a+b)
+
+#N2
+a = 10
+b = 5
+
+addition_result = a + b
+subtraction_result = a - b
+multiplication_result = a * b
+division_result = a / b
+
+# Print the results
+print("Addition:", addition_result)
+print("Subtraction:", subtraction_result)
+print("Multiplication:", multiplication_result)
+print("Division:", division_result)
+
+
+#N3
 try:
     radius = float(input("Enter Radius:"))
     area = math.pi * radius ** 2
@@ -15,6 +30,7 @@ except:
     print(" ENTER FLOAT")
 
 
+#N4
 try:
     minute = float(input("Enter Minute:"))
     second = minute * 60
@@ -23,6 +39,7 @@ except:
     print("ENTER MINUTE")
 
 
+#N5
 host_address = ipaddress.IPv4Address('192.168.0.3')
 network = ipaddress.IPv4Network(f'{host_address}/24', strict=False)
 for ip in network:
